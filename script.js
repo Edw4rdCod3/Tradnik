fef12d897088b57a0f7f977794544c839326938b
Edw4rdCod3 <97491797+Edw4rdCod3@users.noreply.github.com>
2022-08-18 04:28:03 +0300

    update

--- script.js
+++ script.js
@@ -35,3 +35,33 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+
+
+
+
+
+
+
+window.addEventListener('scroll', reveal);
+
+function reveal()
+{
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++)
+  {
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint)
+    {
+      reveals[i].classList.add('active');
+    }
+    else
+    {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
